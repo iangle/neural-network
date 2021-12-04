fb9da79815eb73fef8917c5e8c6bd1690735510d
unknown <iangle@ewu.edu>
2021-12-03 16:01:01 -0800

    added an iniatilize memory and an iniatilize neural network function

--- neuralNetworkGPU.cu
+++ neuralNetworkGPU.cu
@@ -1,13 +1,22 @@
 #include "neuralNetworkGPU.h"
 
-NeuralNetworkGPU::NeuralNetworkGPU(float learningRate)
+NeuralNetworkGPU::NeuralNetworkGPU(int numNeuronInput, int numNeuronHidden, int numNeuronOutput, float learningRate)
 {
     learning_rate = learningRate;
+
+    _numNeuronInput = numNeuronInput;
+    _numNeuronHidden = numNeuronHidden;
+    _numNeuronOut = numNeuronOutput;
+
+    allocateMemoryCPU();
+
+    initializeNN();
+
 }
 
 __device__ Sigmoid(float x) {return 1.0f / (1.0f + exp(-x)); }
 
-__global__ void hiddenLayerForward(float* Z, float* A, int Z_x_dim, int Z_y_dim)
+/*__global__ void hiddenLayerForward(float* Z, float* A, int Z_x_dim, int Z_y_dim)
 {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
 
@@ -47,9 +56,41 @@ Matrix& NeuralNetworkGPU::backPropagation(Matrix& dA)
     hiddenLayerBackPropagation<<<num_blocks, block_size>>>(Z.data_device.get(), dA.data_device.get(), dZ.data_device.get(), Z.shape.x, Z.shape.y);
 
     return dZ;
-}
+}*/
 
 void NeuralNetworkGPU::initializeNN()
 {
+    //initialize our random seed
+    srand ((unsigned)time(NULL));
 
+    //initialize the hidden layer with random numbers between (-.5,.5)
+    for(int i = 0; i < _numNeuronHidden; i++)
+    {
+        weightsHidden[i] = static_cast<float>((rand() % 10000 + 1 - 5000)) / 10000.0f;
+
+        for(int j = 1; j < _numNeuronInput + 1; j++)
+            weightsHidden[i + (j * _numNeuronHidden)] = static_cast<float>((rand() % 10000 + 1 - 5000)) / 10000.0f;
+        
+    }
+
+    //initialize the output layer with random numbers between (-.5,.5)
+    for(int i = 0; i < _numNeuronOut; i++)
+    {
+        weightsOut[i] = static_cast<float>((rand() % 10000 + 1 - 5000)) / 10000.0f;
+
+        for(int j = 1; j < _numNeuronHidden + 1; j++)
+            weightsOut[i + (j * _numNeuronOut)] = static_cast<float>((rand() % 10000 + 1 - 5000)) / 10000.0f;
+
+    }
+}
+
+void NeuralNetworkGPU::allocateMemoryCPU()
+{
+    //these two are 2D arrays that are flattened into a 1D array
+    weightsHidden = (float*) malloc(_numNeuronHidden * (_numNeuronInput + 1) * sizeof(float));
+    weightsOut = (float*) malloc(_numNeuronOut * (_numNeuronHidden + 1) * sizeof(float));
+
+    //these are 1D arrays
+    valuesHidden = (float*) malloc(_numNeuronHidden * sizeof(float));
+    valuesOut = (float*) malloc(_numNeuronOut * sizeof(float));
 }
--- neuralNetworkGPU.h
+++ neuralNetworkGPU.h
@@ -10,20 +10,34 @@ class NeuralNetworkGPU
 {
 private:
 
-    Matrix A;
-    Matrix Z;
-    Matrix dz;
+    //these are the values and the weights of the hidden layer and output layer
+    float* weightsHidden;
+    float* weightsOut;
+    float* valuesHidden;
+    float* valuesOut;
 
+    //the learning rate of the backpropagation of the neural network
     float learning_rate;
+
+    //functions to initialize the neural network and to allocate memory of the layers
+    void initializeNN();
+    void allocateMemoryCPU();
+
+    //the size of the neural network layers
+    int _numNeuronInput = 0;
+    int _numNeuronHidden = 0;
+    int _numNeuronOut = 0;
 
 public:
 
-    NeuralNetworkGPU(float learningRate);
+    //default constructor
+    NeuralNetworkGPU(int numNeuronInput, int numNeuronHidden, int numNeuronOutput, float learningRate);
 
+    //forward and backward propagation through the network
     Matrix& forward(Matrix& Z);
     Matrix& backPropagation(Matrix& dA);
 
-    void test();
+    //train the neural network
     void train();
 
 };
