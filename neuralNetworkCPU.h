8befe5f05ef552334b93180eb22188ba4b7f3b95
unknown <iangle@ewu.edu>
2021-12-05 15:12:54 -0800

    got rid of the private values in nnCPU

--- neuralNetworkCPU.h
+++ neuralNetworkCPU.h
@@ -13,17 +13,6 @@ class neuralNetworkCPU
     
     template<class T> class bpNeuralNetwork
     {
-        private:
-            int numNeuronIn_;
-	        int numNeuronHidden_;
-	        int numNeuronOut_;
-
-	        T indata_[numIn_];								//the input data for the input layer
-	        float wHidden_[numH_][numIn_ + 1];			                //the weight belongs to Hidden Layer
-	        float wOut_[numOut_][numH_ + 1];							//the weight belongs to Output Layer
-	        float vHidden_[numH_];							//the value of Neuron in Hidden Layer
-	        float vOut_[numOut_];								//the value of Neuron in Output Layer
-
 	    public:
             bpNeuralNetwork(int nIn, int nH, int nOut);
             bpNeuralNetwork(const bpNeuralNetwork& initBP);
